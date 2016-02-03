ddf16e8053862a1d30952a3847460670c895c251
coseyo <phpseyo@qq.com>
2016-02-03 11:09:48 +0800

    fix sort bug

--- sortmap/sortmap.go
+++ sortmap/sortmap.go
@@ -1,6 +1,11 @@
 package sortmap
 
-import "sort"
+import (
+	"crypto/md5"
+	"fmt"
+	"reflect"
+	"sort"
+)
 
 type sortMap struct {
 	Key   string
@@ -21,14 +26,35 @@ func (this SortMaps) Less(i, j int) bool {
 	return this[i].Key < this[j].Key
 }
 
-func (this SortMaps) Sort(params map[string]interface{}) map[string]interface{} {
+func (this SortMaps) Sort(params map[string]interface{}) SortMaps {
 	for k, v := range params {
+		vValue := reflect.ValueOf(v)
+
+		if vValue.Kind() == reflect.Map {
+			vValueI := vValue.Interface()
+			ns := &SortMaps{}
+			v = ns.Sort(vValueI.(map[string]interface{}))
+		}
+
 		this = append(this, sortMap{Key: k, Value: v})
 	}
 	sort.Sort(this)
-	m := map[string]interface{}{}
-	for _, v := range this {
-		m[v.Key] = v.Value
+
+	return this
+}
+
+func MapToMD5(params map[string]interface{}) []byte {
+
+	smStruct := &SortMaps{}
+	sm := smStruct.Sort(params)
+	s := ""
+
+	for _, v := range sm {
+		s += fmt.Sprintf("%s=%v&", v.Key, v.Value)
 	}
-	return m
+
+	ctMd5 := md5.New()
+	ctMd5.Write([]byte(s))
+	return ctMd5.Sum(nil)
+
 }
--- sortmap/sortmap_test.go
+++ sortmap/sortmap_test.go
@@ -11,8 +11,23 @@ func TestSortMaps(t *testing.T) {
 		"cc": 22,
 		"dd": "asdf",
 		"bb": "dfe",
+		"ff": map[string]interface{}{
+			"ff3": 1,
+			"ff2": 2,
+			"ff4": 3,
+			"ff1": map[string]interface{}{
+				"ff1_a": 1,
+				"ff1_e": 2,
+				"ff1_d": 3,
+				"ff1_b": 4,
+			},
+		},
+		"ee": 2231,
 	}
 	var m SortMaps
 	p := m.Sort(params)
 	fmt.Println(p)
+
+	md5 := MapToMD5(params)
+	fmt.Println(md5)
 }
